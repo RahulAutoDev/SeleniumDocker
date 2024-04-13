4c279e1fb5d2d07ac4b8c7a92298c3734dcd218c
Rahul Singh <64746581+RahulAutoDev@users.noreply.github.com>
2024-04-14 01:25:24 +0530

    commit -m "docker compose file updated"

--- src/test/java/com/rahulautodev/tests/AbstractTest.java
+++ src/test/java/com/rahulautodev/tests/AbstractTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -28,15 +29,23 @@ public abstract class AbstractTest {
     }
 
     private WebDriver getRemoteDriver() throws MalformedURLException {
-        Capabilities cap;
-        if(System.getProperty("browser").equalsIgnoreCase("chrome"))
-        {
-            cap = new ChromeOptions();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        ChromeOptions option = null;
+        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            option = new ChromeOptions();
+            option.addArguments("--headless");
+            option.addArguments("--disable-gpu");
+            option.addArguments("--window-size=1920,1200");
+            option.addArguments("--ignore-certificate-errors");
+            option.addArguments("--silent");
+            option.addArguments("--remote-allow-origins=*");
+            option.addArguments("--no-sandbox");
+            option.addArguments("--disable-dev-shm-usage");
+            option.merge(cap);
+        } else {
+            FirefoxOptions options = new FirefoxOptions();
         }
-        else{
-            cap = new FirefoxOptions();
-        }
-        return new RemoteWebDriver(new URL("http://143.110.244.31:4444/grid/console"), cap);
+        return new RemoteWebDriver(new URL("http://143.110.244.31:4444/grid/console"), option);
     }
 
     private WebDriver getLocalDriver()
